81db544174030a51cb693963d7d4a52d08861942
Md. Newaj Shahriar <mail4shahriar@gmail.com>
2020-05-12 02:47:21 +0600

    msg div edited

--- lookup/views.py
+++ lookup/views.py
@@ -12,7 +12,30 @@ def home(request):
 		api = "Error..."
 
 
-	return render(request, 'home.html', {'api': api})
+	if api[0]['Category']['Name'] == "Good":
+		category_details = "(0-50) Considered satisfactory"
+		category_color = "good"
+	elif api[0]['Category']['Name'] == "Moderate":
+		category_details = "(51-100) Considered acceptable"
+		category_color = "moderate"
+	elif api[0]['Category']['Name'] == "Unhealthy for Sensitive Groups":
+		category_details = "(101-150) Considered less acceptable"
+		category_color = "usg"
+	elif api[0]['Category']['Name'] == "Unhealthy":
+		category_details = "(151-200) Begin to experience health effects"
+		category_color = "unhealthy"
+	elif api[0]['Category']['Name'] == "Very Unhealthy":
+		category_details = "(201-250) Bad weather"
+		category_color = "veryunhealthy"
+	elif api[0]['Category']['Name'] == "Hazardous":
+		category_details = "(251-300) Very bad weather"
+		category_color = "hazardous"
+
+	return render(request, 'home.html', {
+		'api': api, 
+		'category_details': category_details, 
+		'category_color' : category_color
+		})
 
 def about(request):
 	return render(request, 'about.html', {})
